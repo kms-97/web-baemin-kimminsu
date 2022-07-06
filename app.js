25baa0cb8d9aeba28b18c656aec30e3bd8e54076
kms-97 <msg016@naver.com>
2022-07-06 20:26:20 +0900

    feat: 메인페이지 렌더링

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Test', message: 'Hello, world!'});
+    res.render('index');
 })
 
 app.listen(3000);
