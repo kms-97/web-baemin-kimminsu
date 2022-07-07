c7b8051b0977f147db3d83e6694aa5a48743c67b
unknown <msg016@naver.com>
2022-07-07 22:08:52 +0900

    feat: 닉네임 확인

--- public/js/detail.js
+++ public/js/detail.js
@@ -3,6 +3,8 @@
     const $emailInput = document.getElementById('email-input');
     const $emailValidMark = document.getElementById('email-validate');
     const $additionalInfo = document.getElementById('additional');
+    const $nicknameInput = document.getElementById('nickname-input');
+    const $nicknameValidMark = document.getElementById('nickname-validate');
 
     $emailDuplicationCheckBtn.addEventListener('click', () => {
         const email = $emailInput.value;
@@ -10,5 +12,11 @@
 
         $emailValidMark.style.display = 'block';
         $additionalInfo.style.display = 'block';
+    })
+
+    $nicknameInput.addEventListener('change', () => {
+        const nickname = $nicknameInput.value;
+        if (nickname) $nicknameValidMark.style.display = 'block';
+        else $nicknameValidMark.style.display = 'none';
     })
 })();
