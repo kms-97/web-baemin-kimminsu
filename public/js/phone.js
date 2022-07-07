120bc19883ac4d5b884781546c71d0d4c2907c28
unknown <msg016@naver.com>
2022-07-07 16:18:42 +0900

    feat: 전화번호 포맷 확인 함수 추가

--- public/js/phone.js
+++ public/js/phone.js
@@ -6,4 +6,8 @@
     function insertHyhpen(str) {
         return str.replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/\-*$/, '');
     }
+
+    function isPhoneNumberFormat(str) {
+        return /^\d{3}-\d{4}-\d{4}$/.test(str);
+    }
 })();
