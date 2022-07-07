4c4b8759d0400715946544988723b2b2ecac6fa6
unknown <msg016@naver.com>
2022-07-07 16:15:53 +0900

    feat: 하이픈 삽입 함수 추가

--- public/js/phone.js
+++ public/js/phone.js
@@ -2,4 +2,8 @@
     function removeNotNumberChar(str) {
         return str.replace(/\d/g, '');
     }
+
+    function insertHyhpen(str) {
+        return str.replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/\-*$/, '');
+    }
 })();
